195e875936015471279d6b4917f91220b6d235c5
derushio <derushio@gmail.com>
2019-11-30 18:39:50 +0900

    <master>[modified]

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cpuStats from 'cpu-stats';
 
 async function asyncCpuStats() {
     return await new Promise<CpuStatsResult[]>((resolve, reject) => {
-        cpuStats(1000, (e, results) => {
+        cpuStats(5000, (e, results) => {
             if (e) { reject(e); }
             resolve(results);
         });
